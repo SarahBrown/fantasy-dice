2e851fd0f40bae3e2a29a51c797bc25c4db378ef
Kevin Robb <kevin@js-x.com>
2021-02-06 18:57:27 -0600

    Convert to using Flask. Enables easier function calls & emits as well as image streaming.

--- server/main.py
+++ server/main.py
@@ -1,16 +1,40 @@
-import eventlet
-import socketio
+from makeup_artist import Makeup_artist
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, emit
+from camera import Camera
+from utils import base64_to_pil_image, pil_image_to_base64
+#import eventlet
+#import socketio
 from controllers.roll_tracker import Roll_tracker
 
-sio = socketio.Server(cors_allowed_origins='*')
-app = socketio.WSGIApp(sio)
+# sio = socketio.Server(cors_allowed_origins='*')
+# app = socketio.WSGIApp(sio)
 
-@sio.event
+app = Flask(__name__)
+socketio = SocketIO(app)
+camera = Camera(Makeup_artist())
+
+@socketio.on('connect')
 def connect(sid, environ): 
     print('connect ', sid)
     # users will be asked their name before being allowed to connect
 
-@sio.event
+@socketio.on('dndbeyond_url')
+def get_char_sheet(sid, url:str):
+    # identify the campaign to which the player belongs
+    campaign_id = Roll_tracker.player_in_campaign[sid]
+    # we need the numbers at the end of the URL
+    # make sure there isn't a / at the very end
+    if url[-1] == "/":
+        url = url[:len(url)-1]
+    # strip out the user ID
+    user_id = int(url[url.rfind("/"):])
+    # TODO call Noah's script to generate a JSON char sheet from their 
+    #   DND Beyond profile, and emit it back to the client.
+    char_sheet_json = None
+    emit('char_sheet', {'char_sheet': char_sheet_json}, room=campaign_id)
+
+@socketio.on('init_roll')
 # when the client presses a button to initiate a roll,
 # the client code uses the char sheet to tell them what dice to roll.
 # it then tells the server (us) that a roll is happening, and we 
@@ -23,7 +47,8 @@ def init_roll(sid, roll_purpose:str, roll_modifier:int):
     # identify the campaign to which the player belongs
     campaign_id = Roll_tracker.player_in_campaign[sid]
     # return the link to the stream to all players in that campaign
-    sio.emit('stream_link', stream_link, campaign=campaign_id)
+    #sio.emit('stream_link', stream_link, room=campaign_id)
+    emit('stream_link', {'stream_link': stream_link}, room=campaign_id)
     # TODO send a request to openCV stuff and wait for a reply with the result
     roll_result = -1
 
@@ -31,14 +56,15 @@ def init_roll(sid, roll_purpose:str, roll_modifier:int):
     # and passed to this method.
     roll_result += roll_modifier
     # send the result of the roll to the client for a special display
-    sio.emit('new_roll_result', roll_result, campaign=campaign_id)
+    #sio.emit('new_roll_result', roll_result, campaign=campaign_id)
+    emit('new_roll_result', {'roll_result': roll_result}, room=campaign_id)
     # add the new roll to the campaign's roll history
     Roll_tracker.receive_roll(sid, roll_purpose, roll_result)
     # send new roll history to update the chat display
-    sio.emit('roll_history', Roll_tracker.get_roll_history(campaign_id), campaign=campaign_id)
+    #sio.emit('roll_history', Roll_tracker.get_roll_history(campaign_id), room=campaign_id)
+    emit('roll_history', {'roll_history': Roll_tracker.get_roll_history(campaign_id)}, room=campaign_id)
 
-
-@sio.event
+@socketio.on('join_campaign')
 # campaigns are rooms
 def join_campaign(sid, name, campaign_id):
     # make sure player name and campaign are alphanumeric and within the allowed lengths
@@ -49,23 +75,30 @@ def join_campaign(sid, name, campaign_id):
         # create player and assign to campaign
         Roll_tracker.join_campaign(sid, name, campaign_id)
         # put up the roll history and list of players
-        sio.enter_campaign(sid, campaign_id)
-        sio.emit('joined_campaign', campaign=campaign_id, player=sid)
-        sio.emit('player_list', Roll_tracker.get_player_list(campaign_id), campaign=campaign_id)
-        sio.emit('roll_history', Roll_tracker.get_roll_history(campaign_id), campaign=campaign_id)
+        sio.join_room(sid, campaign_id)
+        #sio.emit('joined_campaign', campaign=campaign_id, player=sid)
+        emit('joined_campaign', {'campaign_id': campaign_id, "player": sid}, room=campaign_id)
+        #sio.emit('player_list', Roll_tracker.get_player_list(campaign_id), room=campaign_id)
+        emit('player_list', {'player_list': Roll_tracker.get_player_list(campaign_id)}, room=campaign_id)
+        #sio.emit('roll_history', Roll_tracker.get_roll_history(campaign_id), room=campaign_id)
+        emit('roll_history', {'roll_history': Roll_tracker.get_roll_history(campaign_id)}, room=campaign_id)
 
-@sio.event
+@socketio.on('disconnect')
 def disconnect(sid):
     print('disconnect ', sid)
     # remove person from the list
     # important so refreshing does not keep increasing the number of people
     if sid in Roll_tracker.player_in_campaign:
         campaign_id = Roll_tracker.player_in_campaign[sid]
-        sio.leave_campaign(sid, campaign_id) # TODO check this works
+        #sio.leave_room(sid, campaign_id) # TODO check this works
         Roll_tracker.remove_player(sid)
         # update the player list display
-        sio.emit('player_list', Roll_tracker.get_player_list(campaign_id), campaign=campaign_id)
-        
+        #sio.emit('player_list', Roll_tracker.get_player_list(campaign_id), room=campaign_id)
+        emit('player_list', {'player_list': Roll_tracker.get_player_list(campaign_id)}, room=campaign_id)
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    #eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    socketio.run(app)
+
+# Referenced the following repo to convert socket IO to work with Flash:
+# https://github.com/dxue2012/python-webcam-flask/blob/master/app.py
